cfb62113662ac988386ff4795e651b70c3f8cf19
你爹 <@XXX666>
2020-03-20 20:36:36 +0800

    传给Blackdog

---
+++ ECommerceApi/MODEL/XuYaDon/OrderModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.XuYaDon
+{
+    /// <summary>
+    /// 订单表
+    /// </summary>
+  public  class OrderModel
+    {
+    }
+}
