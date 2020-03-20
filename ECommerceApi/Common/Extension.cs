839b37bf6e4200aaa9240015255d1097e5b5167a
康家齐11 <1965623798@qq.com>
2020-03-20 19:22:40 +0800

    jwt安全验证

---
+++ ECommerceApi/Common/Extension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public static class Extension
+    {
+        /// <summary>
+        /// 时间转换为时间戳
+        /// </summary>
+        /// <param name="dateTime"></param>
+        /// <returns></returns>
+        public static long ToUnixTime(this DateTime dateTime)
+        {
+            System.DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new System.DateTime(1970, 1, 1, 0, 0, 0, 0));
+            long t = 0;
+            //秒
+            t = (long)(dateTime - startTime).TotalSeconds;
+
+            return t;
+        }
+    }
+}
