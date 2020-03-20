3ddbee4df443fd11afe5cae2eab101cc69aed57a
康家齐11 <1965623798@qq.com>
2020-03-20 11:50:43 +0800

    框架

--- ECommerceApi/SDCKClient/BaseRequest.cs
+++ ECommerceApi/SDCKClient/BaseRequest.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace SDCKClient
 {
-    class BaseRequest
+    /// <summary>
+    /// 请求的基类消息
+    /// 表示，你要调用的我的接口，你就得吧这些传给我
+    /// </summary>
+    public abstract class BaseRequest
     {
+        /// <summary>
+        /// token参数，做验证用
+        /// </summary>
+        public string Token { get; set; }
+        /// <summary>
+        /// apiurl
+        /// </summary>
+        /// <returns></returns>
+        public abstract string GetApiName();
     }
 }
--- ECommerceApi/SDCKClient/BaseResponse.cs
+++ ECommerceApi/SDCKClient/BaseResponse.cs
@@ -6,7 +6,61 @@ using System.Threading.Tasks;
 
 namespace SDCKClient
 {
+    public class BaseResponseT<T>
+    {
+        #region 表示请求相关的
+        /// <summary>
+        /// 状态
+        /// 表示请求状态
+        /// 默认为1 表示正常
+        /// 为-1时 表示错误
+        /// </summary>
+        public int Status = 1;
+        /// <summary>
+        /// 消息
+        /// 指的时返回的一些消息消息 当状态-1 时显示此消息
+        /// </summary>
+        public string Msg { get; set; }
+        #endregion
+        #region 业务相关的返回
+        /// <summary>
+        /// 一种状态返回
+        /// 比如 ，添加是否成功，删除是否成功
+        /// </summary>
+        public bool IsSuccess { get; set; }
+        /// <summary>
+        ///返回一条数据 比如说查询一条消息
+        /// </summary>
+        public T Info { get; set; }
+        /// <summary>
+        /// 返回一个集合 比如下课绑定
+        /// </summary>
+        public List<T> DateList { get; set; }
+        #endregion
+
+    }
     public class BaseResponse
     {
+        #region 表示请求相关的
+        /// <summary>
+        /// 状态
+        /// 表示请求状态
+        /// 默认为1 表示正常
+        /// 为-1时 表示错误
+        /// </summary>
+        public int Status = 1;
+        /// <summary>
+        /// 消息
+        /// 指的时返回的一些消息消息 当状态-1 时显示此消息
+        /// </summary>
+        public string Msg { get; set; }
+        #endregion
+        #region 业务相关的返回
+        /// <summary>
+        /// 一种状态返回
+        /// 比如 ，添加是否成功，删除是否成功
+        /// </summary>
+        public bool IsSuccess { get; set; }
+        #endregion
     }
 }
