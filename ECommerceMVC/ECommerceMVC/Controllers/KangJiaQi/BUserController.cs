2d7dd1eafba1f21b7ed057318d0cfa9ba7b0c23e
康家齐11 <1965623798@qq.com>
2020-03-21 16:18:58 +0800

    首页样式

---
+++ ECommerceMVC/ECommerceMVC/Controllers/KangJiaQi/BUserController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceMVC.Controllers.KangJiaQi
+{
+    public class BUserController : Controller
+    {
+        /// <summary>
+        /// 首页
+        /// </summary>
+        /// <returns></returns>
+        // GET: BUser
+        public ActionResult Index()
+        {
+            return View();
+        }
+        /// <summary>
+        /// 登录页面
+        /// </summary>
+        /// <returns></returns>
+        public ActionResult UserLogin()
+        {
+            return View();
+        }
+    }
+}
