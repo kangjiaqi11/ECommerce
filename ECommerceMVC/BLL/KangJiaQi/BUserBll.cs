d9393c3df1edcb8da44f6893d393a123784d38a6
康家齐11 <1965623798@qq.com>
2020-03-21 16:54:58 +0800

    jwt

---
+++ ECommerceMVC/BLL/KangJiaQi/BUserBll.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SDCKClient;
+namespace BLL
+{
+    /// <summary>
+    /// 用户相关的mvcbll
+    /// </summary>
+   public class BUserBll
+    {
+        public BUserLoginResponse<int> UserLogin(BUserLoginRequst bUserLoginRequst)
+        {
+            return ApiRequestHelper.Post<BUserLoginRequst, BUserLoginResponse<int>>(bUserLoginRequst);
+        }
+    }
+}
