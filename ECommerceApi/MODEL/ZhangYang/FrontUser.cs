05eba282b9be33ea22455ad1cab536e47c02958a
zhangyang08075414 <59247968+zhangyang08075414@users.noreply.github.com>
2020-03-23 14:11:38 +0800

    model

---
+++ ECommerceApi/MODEL/ZhangYang/FrontUser.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.ZhangYang
+{
+    public class FrontUser
+    {
+        //用户id
+        public int FrontUserid { get; set; }
+        //用户账户
+        public string Account        { get; set; }
+        //用户密码
+        public string UserPwd        { get; set; }
+        //用户昵称
+        public string UserName       { get; set; }
+        //会员等级
+        public string MembersId      { get; set; }
+        //消费金额
+        public string Consumption    { get; set; }
+        //订单数量
+        public int Number         { get; set; }
+        //用户职业
+        public string professional   { get; set; }
+        //用户性别
+        public string Age            { get; set; }
+        //城市
+        public string city           { get; set; }
+        //生日
+        public string Baddy          { get; set; }
+        //喜欢分类
+        public string classification { get; set; }
+        //个性签名
+        public string Grqm           { get; set; }
+
+        //商品评价
+        public string Sppj           { get; set; }
+        //关注
+        public string Gz             { get; set; }
+        //粉丝
+        public string Fs             { get; set; }
+        //收藏商品
+        public string Sc             { get; set; }
+        //退货记录
+        public string Thjl           { get; set; }
+        //成长值
+        public int Czz            { get; set; }
+        //优惠卷
+        public string Yhj            { get; set; }
+        //收藏话题
+        public string Scht           { get; set; }
+        //订单id
+        public int Ddid           { get; set; }
+        //收货地址id
+        public int ShdzId         { get; set; }
+        public int State          { get; set; }
+        public DateTime CTime          { get; set; }
+        public DateTime XTime          { get; set; }
+        public int Modifier       { get; set; }
+        public int Founder        { get; set; }
+    }
+}
---
+++ ECommerceApi/MODEL/ZhangYang/Member.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MODEL.ZhangYang
+{
+    public class Member
+    {
+        //会员id
+        public int MembersId  { get; set; }
+        //会员名称
+        public string members    { get; set; }
+        //成长值满电
+        public string Growth     { get; set; }
+        //评价奖励
+        public string Evaluation { get; set; }
+        //免运费标准
+        public string Freight    { get; set; }
+        //备注
+        public string Note       { get; set; }
+        //数据状态
+        public string State      { get; set; }
+        //创建时间
+        public string CTime      { get; set; }
+        //修改时间
+        public string XTime      { get; set; }
+        //创建人
+        public int Founder    { get; set; }
+        //修改人
+        public int UpdateId   { get; set; }
+    }
+}
