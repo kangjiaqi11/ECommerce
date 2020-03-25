a352489a53a214bf8133a7f81793c4fe3ab97be6
你爹 <@XXX666>
2020-03-25 11:48:59 +0800

    更新了xuyadong

---
+++ ECommerceApi/BLL/XuYaDon/OrderBll.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dal
+namespace BLL.XuYaDon
+{
+    /// <summary>
+    /// 订单BLL
+    /// </summary>
+   public class OrderBll
+    {
+
+    }
+}
--- ECommerceApi/MODEL/XuYaDon/OrderModel.cs
+++ ECommerceApi/MODEL/XuYaDon/OrderModel.cs
@@ -87,5 +87,57 @@ namespace MODEL.XuYaDon
         /// 修改人
         /// </summary>
         public int EditId { get; set; }
+        /// <summary>
+        /// 商品名称
+        /// </summary>
+        public string GoodsName { get; set; }
+        /// <summary>
+        /// 商品品牌号
+        /// </summary>
+        public int BrandId { get; set; }
+        /// <summary>
+        /// 品牌名称
+        /// </summary>
+        public string BrandName { get; set; }
+        /// <summary>
+        /// 商品类型名称
+        /// </summary>
+        public string TypeName { get; set; }
+        /// <summary>
+        /// 销售价格
+        /// </summary>
+        public decimal MarketPrice { get; set; }
+        /// <summary>
+        /// 前台账号
+        /// </summary>
+        public string Account { get; set; }
+        /// <summary>
+        /// 收货人手机号
+        /// </summary>
+        public string Photo { get; set; }
+        /// <summary>
+        /// 收货人名称
+        /// </summary>
+        public string Consignee { get; set; }
+        /// <summary>
+        /// 收货人邮编
+        /// </summary>
+        public string Email { get; set; }
+        /// <summary>
+        /// 收货人详细地址
+        /// </summary>
+        public string Address { get; set; }
+        /// <summary>
+        /// 发票类型
+        /// </summary>
+        public string InvoieTypeName { get; set; }
+        /// <summary>
+        /// 发票抬头
+        /// </summary>
+        public string InvoiceHead { get; set; }
+        /// <summary>
+        /// 发票内容
+        /// </summary>
+        public string Invoicecontent { get; set; }
     }                     
 }
