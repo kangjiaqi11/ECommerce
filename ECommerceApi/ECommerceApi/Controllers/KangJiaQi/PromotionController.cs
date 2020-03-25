b7cd5ecd97ba8b579823443b9c252cc056b2f271
康家齐11 <1965623798@qq.com>
2020-03-25 11:57:15 +0800

    显示

---
+++ ECommerceApi/ECommerceApi/Controllers/KangJiaQi/PromotionController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using SDCKClient;
+using BLL;
+namespace ECommerceApi.Controllers.KangJiaQi
+{
+    public class PromotionController : ApiController
+    {
+        promotionBll promotionBll = new promotionBll();
+        /// <summary>
+        /// 秒杀活动显示
+        /// </summary>
+        /// <param name="activityShowRequst"></param>
+        /// <returns></returns>
+        public ActivityShowRespnse ActivityShow(ActivityShowRequst activityShowRequst)
+        {
+            return promotionBll.ActivityShow(activityShowRequst);
+        }
+    }
+}
---
+++ ECommerceMVC/ECommerceMVC/Controllers/KangJiaQi/promotionController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ECommerceMVC.Controllers.KangJiaQi
+{
+    public class promotionController : Controller
+    {
+        // GET: promotion
+        public ActionResult Show()
+        {
+            return View();
+        }
+    }
+}
