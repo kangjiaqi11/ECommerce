2df5ef8e8d3c5dc9e4489da6149cda3e5ae8fac4
zhangyang08075414 <59247968+zhangyang08075414@users.noreply.github.com>
2020-03-26 14:47:41 +0800

    fenye视图

---
+++ ECommerceMVC/BLL/ZhangYang/StoreroomBLL.cs
@@ -0,0 +1,18 @@
+﻿using SDCKClient.ZhangYang.Request;
+using SDCKClient.ZhangYang.Response;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.ZhangYang
+{
+    public class StoreroomBLL
+    {
+        public StoreroomShowResp StoreroomFenye(StoreroomShowReq ssr,string GetApiName)
+        {
+            return ApiRequestHelper.Post<StoreroomShowReq,StoreroomShowResp>(ssr, GetApiName);
+        }
+    }
+}
