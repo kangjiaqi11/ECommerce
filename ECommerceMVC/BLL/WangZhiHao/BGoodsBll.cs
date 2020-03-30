d57587c1bbae538f86b60d2f8db55728cff226bc
wzh-123123 <61002157+wzh-123123@users.noreply.github.com>
2020-03-30 21:37:28 +0800

    wzh

--- ECommerceMVC/BLL/WangZhiHao/BGoodsBll.cs
+++ ECommerceMVC/BLL/WangZhiHao/BGoodsBll.cs
@@ -1,13 +1,26 @@
-﻿using System;
+﻿using SDCKClient;
+using SDCKClient.WangZhiHao.Request;
+using SDCKClient.WangZhiHao.Response;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace BLL.WangZhiHao
+namespace BLL
 {
     public class BGoodsBll
     {
-
+        
+        /// <summary>
+        /// 显示
+        /// </summary>
+        /// <param name="goodsAddRequest"></param>
+        /// <param name="GetApiName"></param>
+        /// <returns></returns>
+        public GoodsAddResponse GoodsAdd(GoodsAddRequest goodsAddRequest,string GetApiName)
+        {
+            return ApiRequestHelper.Post<GoodsAddRequest, GoodsAddResponse>(goodsAddRequest, GetApiName);
+        }
     }
 }
